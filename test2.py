06dcb070d86f7385f8042809308b1fb7bd75fd12
yuxiang <1020754861@qq.com>
2018-04-20 22:56:13 +0800

    add a new file

---
+++ test2.py
@@ -0,0 +1,21 @@
+def add_end(L=None):
+    if L is None:
+        L=[]
+    L.append('END')
+    return L
+add_end()
+
+def calc(numbers):
+    sum=0
+    for n in numbers:
+        sum=sum+n*n
+    return sum
+calc([1,2,3])
+
+def calc(*numbers):
+    sum=0
+    for n in numbers:
+        sum=sum+n*n
+    return sum
+
+calc(1,2,3)
