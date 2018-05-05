4bc893e03166caf29f69fa16ebd308afaa1e45b3
yuxiang <1020754861@qq.com>
2018-05-05 14:27:34 +0800

    replace

--- ID3.py
+++ ID3.py
@@ -1,5 +1,105 @@
-for i, value in enumerate(['A','B','C']):
-     print(i,value)
+import math
+import operator
 
 
-[x*x for x in range(1,11) if x%2==0]
+def calcShannonEnt(dataset):
+    numEntries = len(dataset)
+    labelCounts = {}
+    for featVec in dataset:
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
+
+    shannonEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key]) / numEntries
+        shannonEnt -= prob * math.log(prob, 2)
+    return shannonEnt
+
+
+def CreateDataSet():
+    dataset = [['youth', 'no', 'no', '1', 'refuse'],
+               ['youth', 'no', 'no', '2', 'refuse'],
+               ['youth', 'yes', 'no', '2', 'agree'],
+               ['youth', 'yes', 'yes', '1', 'agree'],
+               ['youth', 'no', 'no', '1', 'refuse'],
+               ['mid', 'no', 'no', '1', 'refuse'],
+               ['mid', 'no', 'no', '2', 'refuse'],
+               ['mid', 'yes', 'yes', '2', 'agree'],
+               ['mid', 'no', 'yes', '3', 'agree'],
+               ['mid', 'no', 'yes', '3', 'agree'],
+               ['elder', 'no', 'yes', '3', 'agree'],
+               ['elder', 'no', 'yes', '2', 'agree'],
+               ['elder', 'yes', 'no', '2', 'agree'],
+               ['elder', 'yes', 'no', '3', 'agree'],
+               ['elder', 'no', 'no', '1', 'refuse'],
+               ]
+    labels = ['age', 'working?', 'house?', 'credit_situation']
+    return dataset, labels
+
+
+def splitDataSet(dataSet, axis, value):
+    retDataSet = []
+    for featVec in dataSet:
+        if featVec[axis] == value:
+            reducedFeatVec = featVec[:axis]
+            reducedFeatVec.extend(featVec[axis + 1:])
+            retDataSet.append(reducedFeatVec)
+
+    return retDataSet
+
+
+def chooseBestFeatureToSplit(dataSet):
+    numberFeatures = len(dataSet[0]) - 1
+    baseEntropy = calcShannonEnt(dataSet)
+    bestInfoGain = 0.0;
+    bestFeature = -1;
+    for i in range(numberFeatures):
+        featList = [example[i] for example in dataSet]
+        uniqueVals = set(featList)
+        newEntropy = 0.0
+        for value in uniqueVals:
+            subDataSet = splitDataSet(dataSet, i, value)
+            prob = len(subDataSet) / float(len(dataSet))
+            newEntropy += prob * calcShannonEnt(subDataSet)
+        infoGain = baseEntropy - newEntropy
+        if (infoGain > bestInfoGain):
+            bestInfoGain = infoGain
+            bestFeature = i
+    return bestFeature
+
+
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys():
+            classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+
+def createTree(dataSet, labels):
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    if len(dataSet[0]) == 1:
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel: {}}
+    del (labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels)
+    return myTree
+
+
+myDat, labels = CreateDataSet()
+tree=createTree(myDat, labels)
+
+print(tree)
+
