0a1010cf26a608a741effde64af1806b9869e7b2
yuxiang <1020754861@qq.com>
2018-05-16 20:58:04 +0800

    replace

--- test4.py
+++ test4.py
@@ -1,6 +1,17 @@
+from functools import reduce
+
+
 for i, value in enumerate(['A','B','C']):
      print(i,value)
 
 
 [x*x for x in range(1,11) if x%2==0]
-a
+
+
+L1=['adam','LISA','barT']
+
+def normalize(name):
+     return name.capitalize()
+
+L2=list(map(normalize,L1))
+print(L2)
