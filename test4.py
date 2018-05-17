b7ba6dacbbd29e5efd3214aa95663ab0b1fdc697
yuxiang <1020754861@qq.com>
2018-05-17 23:55:45 +0800

    replace

--- test4.py
+++ test4.py
@@ -23,6 +23,14 @@ def calc_sum(*args):
         ax=ax+n
     return ax
 
+def lazy_sum(*args):
+    def sum():
+        ax=0
+        for n in args:
+            ax=ax+n
+        return ax
+    return sum
+
 
 
 
